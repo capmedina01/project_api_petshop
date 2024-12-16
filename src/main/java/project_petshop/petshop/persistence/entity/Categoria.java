a95453015bd42119009cb562b5487d680c887d3a
perdomomedina.carlos@gmail.com <perdomomedina.carlos@gmail.com>
2024-12-16 14:06:19 -0500

    productos categoria entidades

--- src/main/java/project_petshop/petshop/persistence/entity/Categoria.java
+++ src/main/java/project_petshop/petshop/persistence/entity/Categoria.java
@@ -1,2 +1,43 @@
-package project_petshop.petshop.persistence.entity;public class Categoria {
+package project_petshop.petshop.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "CATEGORIAS")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "descripcion")
+    private String nombreCategoria;
+
+    private Boolean estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
 }
--- src/main/java/project_petshop/petshop/persistence/entity/Producto.java
+++ src/main/java/project_petshop/petshop/persistence/entity/Producto.java
@@ -1,2 +1,97 @@
-package project_petshop.petshop.persistence.entity;public class Producto {
+package project_petshop.petshop.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "PRODUCTOS")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private int cantidadStock;
+
+    private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
+    private Categoria categoria;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public int getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(int cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
